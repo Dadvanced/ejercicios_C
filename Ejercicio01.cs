cdf743761f2db7652165cf890f088b5f12c011d6
David León Galisteo <dadvanced@hotmail.es>
2017-03-27 12:49:19 +0200

    Añadido Ejercicio01 C#
    Añadido Ejercicio 01 de programación en C#

---
+++ Ejercicio01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            string name;
+
+            Console.WriteLine("Escriba su nombre por favor.");
+            line = Console.ReadLine();
+            name = line;
+
+            Console.WriteLine("Su nombre es: " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
