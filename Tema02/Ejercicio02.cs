d49f915270a6487d79fc1c6c992daad0fce23817
David León Galisteo <dadvanced@hotmail.es>
2017-03-27 13:01:27 +0200

    Añadido Ejercicio02
    Añadido el segundo ejercicio de C# (ejercicio 06 del Tema 2 de programación

---
+++ Tema02/Ejercicio02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Ejercicio02
+    {
+        static void Main(string[] args)
+        {
+            double price = 37.89;
+            int iva = 21;
+
+            double calculateIva = price * iva / 100;
+            double totalPrice = calculateIva + price;
+
+            Console.WriteLine("Mi precio sin IVA es: " + price);
+            Console.WriteLine("El IVA de la factura es: " + calculateIva);
+            Console.WriteLine("La factura total a pagar es: " + totalPrice);
+
+            Console.ReadKey();
+
+        }
+    }
+}
