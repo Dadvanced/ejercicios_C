397b6c690383a8c91898c5e50c6988796fb4d1e1
David León Galisteo <dadvanced@hotmail.es>
2017-04-03 12:06:08 +0200

    Subido ejercicio02 del tema 3

---
+++ Tema03/Ejercicio02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Ejercicio02
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la cantidad de € a convertir: ");
+
+            string line;
+            line = Console.ReadLine();
+            double euros = double.Parse(line);
+
+            double converter = euros * 166.386;
+
+            Console.WriteLine("La cantidad de euros introducida es " + converter + " pesetas.");
+            Console.ReadKey();
+        }
+    }
+}
